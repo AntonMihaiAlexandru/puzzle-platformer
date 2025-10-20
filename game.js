df7b0913bf1aecfeb2afa6217afeff8b4e251c3e
AntonMihaiAlexandru <antonmihai22022006@gmail.com>
2025-10-20 17:14:50 +0300

    File Test

---
+++ game.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "lime";
+ctx.font = "24px sans-serif";
+ctx.fillText("Hello, Puzzle Platformer!", 250, 240);
